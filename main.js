0b83db0143cb372c519a1a7f5e12dace81f4a771
saidolim10 <git001hub@gmail.com>
2024-11-27 21:21:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let numb = ["1", "2", "11", "43", "54", "80", "46" ]
+
+console.log(numb, 'sonlar')
+let juft = []
+
+
+
+
+for (let a = 0; a < numb.length; a++) {
+    const e = numb[a];
+
+    if (e % 2 === 0) {
+        juft.push(e)
+        
+    }
+    else{
+
+    }
+}
+
+console.log(juft, 'juft sonlar');
+
